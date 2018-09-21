10664c5282fb856f77c4d41d00eae92df1047eb6
vvictorious <vvictorious30@gmail.com>
2018-09-20 22:56:14 -0600

    starting to add the logic

---
+++ app.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	var form = document.getElementById('form');
+	var input = document.getElementById('input');
+	var btn = document.getElementById('btn');
+	var list = document.getElementById('list');
+	var id = 1;
+}
